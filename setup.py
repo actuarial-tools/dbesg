cf6b27b818a418ae89737fee640c9f7625ff5657
Lee Sang Jin <lee3jjang@gmail.com>
2020-07-14 23:16:54 +0900

    패키징

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+ 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+ 
+setuptools.setup(
+    name="dbesg",
+    version="0.0.1",
+    author="Lee Sang Jin",
+    author_email="lee3jjang@gmail.com",
+    description="Economic Scenario Generator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lee3jjang/dbesg",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
