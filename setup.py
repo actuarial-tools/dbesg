490a1cc071dd2195d3a3b93cb7d0cb64693914dd
Lee Sang Jin <lee3jjang@gmail.com>
2020-07-14 23:27:29 +0900

    패키지 배포

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
  
-setuptools.setup(
+setup(
     name="dbesg",
     version="0.0.1",
     author="Lee Sang Jin",
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/lee3jjang/dbesg",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
