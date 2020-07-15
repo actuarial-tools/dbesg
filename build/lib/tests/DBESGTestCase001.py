05e72ef6ae9a29ad98a0dcf9367fd89b2bb3aaf9
이상진 <lee3jjang@gmail.com>
2020-07-15 09:07:33 +0900

    테스트 케이스 추가

---
+++ build/lib/tests/DBESGTestCase001.py
@@ -0,0 +1,14 @@
+import unittest
+
+class DBESGTestCase001(unittest.TestCase):
+
+    def setUp(self):
+        """ 테스트 시작 전 """
+        pass
+
+    def tearDown(self):
+        """ 테스트 종료 후 """
+        print("테스트를 종료합니다.")
+
+    def test_runs(self):
+        pass
